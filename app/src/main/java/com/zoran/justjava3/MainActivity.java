18552989bf97ac833d9a99997b2e9c8d5cd953f1
zoran <chenzoran3@gmail.com>
2020-03-02 11:09:59 +0800

    displayQuantity Method Min & Max Add

--- app/src/main/java/com/zoran/justjava3/MainActivity.java
+++ app/src/main/java/com/zoran/justjava3/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     int quantity=0;
     Button orderBtn,minusBtn,plusBtn;
-    CheckBox whippedChk;
+    CheckBox whippedChk,chocolateChk;
     TextView qNum,orderMsg;
     String msgSummary;
     @Override
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         minusBtn = (Button)findViewById(R.id.minus_btn);
         plusBtn = (Button)findViewById(R.id.plus_btn);
         whippedChk = (CheckBox)findViewById(R.id.wCream_chkBox);
+        chocolateChk = (CheckBox)findViewById(R.id.chocTopping_chkBox);
         orderMsg = (TextView)findViewById(R.id.orderMsg_text_view);
         qNum = (TextView)findViewById(R.id.quantityNum_text_view);
     }
@@ -40,14 +41,21 @@ public class MainActivity extends AppCompatActivity {
         getView();
         minOrPlus(view);
     }
+
     private void minOrPlus(View view){
         int number;
         if (view == minusBtn){
             number = Integer.parseInt(qNum.getText().toString());
+            if (number == 1){
+                return;
+            }
             number--;
             qNum.setText(String.valueOf(number));
         }else if (view == plusBtn){
             number = Integer.parseInt(qNum.getText().toString());
+            if (number == 99){
+                return;
+            }
             number++;
             qNum.setText(String.valueOf(number));
         }
